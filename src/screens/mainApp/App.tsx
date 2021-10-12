36b899568ae72d0995cd2f61f2292f99d4f32ead
pankhuri <pankhurig1@gmail.com>
2021-10-12 14:27:16 +0530

    changes

--- src/screens/mainApp/App.tsx
+++ src/screens/mainApp/App.tsx
@@ -7,15 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>New App</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Hi, Pankhuri this side</p>
       </header>
     </div>
   );
